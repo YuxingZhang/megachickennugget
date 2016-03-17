1407ff999ecd23184bfa42574f97a3b9156f501d
Tianshu Ren <tianshu.r@gmail.com>
2016-03-17 15:47:30 -0400

    edited vi.py

--- src/vi.py
+++ src/vi.py
@@ -1,10 +1,16 @@
 import sys
 import numpy as np
 
+# Input: 
+# 	Documents {B_1,...,B_D}
+# Output:
+# 	Variational parameters {u, u_prime, rho, alpha, eta, z}	
+
 def run():
     # Yuxing Zhang TODO
 
     # Tianshu Ren starts here TODO
+
 
 if __name__ == "__main__":
     run()
