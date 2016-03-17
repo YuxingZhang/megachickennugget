67bf5c7fd762e8e6cbd37bc46f7bc6db92c3a684
Tianshu Ren <tianshu.r@gmail.com>
2016-03-17 17:51:51 -0400

    haha

--- src/vi.py
+++ src/vi.py
@@ -15,29 +15,51 @@ def initialize_variables():
 	# initialize Z s.t. Z_dn is a vector of size K as parameters for a categorical distribution
 	Z = list()
 	for d in range(D):
-		Z.append([normalize(np.random.uniform(0,1,(1, K)), 'l1') for i in range(N[d])])
+		Z.append([normalize(np.random.uniform(0, 1, (1, K)), 'l1') for i in range(N[d])])
 	# initialize Eta s.t. Eta_d contains two fields "mu" (1 * K) and "Sigma" (K*K) that specifies a multivariate gaussian
 	Eta = list()
 	for d in range(D):
-		mu_tmp = np.random.rand(1, K)
-		Sigma_tmp = np.diag(np.random.rand(1, K))
-		Eta.append(dict(mu = mu_tmp, Sigma = Sigma_tmp))
+		Eta.append(gen_normalparams(K))
+		# mu_tmp = np.random.rand(1, K)
+		# Sigma_tmp = np.diag(np.random.rand(1, K))
+		# Eta.append(dict(mu = mu_tmp, Sigma = Sigma_tmp))
 	# initialize A s.t. A_d contains two fields "mu" (1 * doc_dim) and "Sigma" (doc_dim * doc_dim) that specifies a multivariate gaussian
 	A = list()
 	for d in range(D):
-		mu_tmp = np.random.rand(1, doc_dim)
-		Sigma_tmp = np.diag(np.random.rand(1, doc_dim))
-		A.append(dict(mu = mu_tmp, Sigma = Sigma_tmp))
+		A.append(gen_normalparams(doc_dim))
+		# mu_tmp = np.random.rand(1, doc_dim)
+		# Sigma_tmp = np.diag(np.random.rand(1, doc_dim))
+		# A.append(dict(mu = mu_tmp, Sigma = Sigma_tmp))
 	# initialize Rho s.t. Rho_k contains two fields "mu" (1 * V) and "Sigma" (V * V) that specifies a multivariate gaussian
 	Rho = list()
 	for k in range(K):
-		mu_tmp = np.random.rand(1, V)
-		Sigma_tmp = np.diag(np.random.rand(1, V))
-		Rho.append(dict(mu = mu_tmp, Sigma = Sigma_tmp))
-	# initialize 
+		Rho.append(gen_normalparams(V))
+		# mu_tmp = np.random.rand(1, V)
+		# Sigma_tmp = np.diag(np.random.rand(1, V))
+		# Rho.append(dict(mu = mu_tmp, Sigma = Sigma_tmp))
+	# initialize U_prime s.t. U_prime_k contains two fields "mu" (1 * word_dim) and "Sigma" (word_dim * word_dim) that specifies a multivariate gaussian
 	U_prime = list()
+	for k in range(K):
+		U_prime.append(gen_normalparams(word_dim))
+		# mu_tmp = np.random.rand(1, word_dim)
+		# Sigma_tmp = np.diag(np.random.rand(1, word_dim))
+		# U_prime.append(dict(mu = mu_tmp, Sigma = Sigma_tmp))
+	# initialize U s.t. U_k contains two fields "mu" (1 * doc_dim) and "Sigma" (doc_dim, doc_dim)
+	U = list()
+	for k in range(K):
+		U.append(gen_normalparams(doc_dim))
+		# mu_tmp = np.random.rand(1, doc_dim)
+		# Sigma_tmp = np.diag(np.random.rand(1, doc_dim))
+		# U.append(dict(mu = mu_tmp, Sigma = Sigma_tmp))
+
 def load_documents(filein):
+	# TODO:
 	return B
+
+def gen_normalparams(dim):
+	mu_tmp = np.random.rand(1, dim)
+	Sigma_tmp = np.diag(np.random.rand(1, dim))
+	return dict(mu = mu_tmp, Sigma = Sigma_tmp)
 
 def run():
 	B = load_documents(filepath)
