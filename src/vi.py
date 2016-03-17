ce2977aae67f91015a53625b70d1311d17f81458
Tianshu Ren <tianshu.r@gmail.com>
2016-03-17 17:38:10 -0400

    hah

--- src/vi.py
+++ src/vi.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
 import update
+from sklearn.preprocessing import normalize
 
 # Input: 
 # 	Documents {B_1,...,B_D}
@@ -11,11 +12,30 @@ import update
 # 	doc_dim:	Dimension of document embedding
 
 def initialize_variables():
-	# initialize z
-	for d in D:
-		for n in N(d):
-			
-
+	# initialize Z s.t. Z_dn is a vector of size K as parameters for a categorical distribution
+	Z = list()
+	for d in range(D):
+		Z.append([normalize(np.random.uniform(0,1,(1, K)), 'l1') for i in range(N[d])])
+	# initialize Eta s.t. Eta_d contains two fields "mu" (1 * K) and "Sigma" (K*K) that specifies a multivariate gaussian
+	Eta = list()
+	for d in range(D):
+		mu_tmp = np.random.rand(1, K)
+		Sigma_tmp = np.diag(np.random.rand(1, K))
+		Eta.append(dict(mu = mu_tmp, Sigma = Sigma_tmp))
+	# initialize A s.t. A_d contains two fields "mu" (1 * doc_dim) and "Sigma" (doc_dim * doc_dim) that specifies a multivariate gaussian
+	A = list()
+	for d in range(D):
+		mu_tmp = np.random.rand(1, doc_dim)
+		Sigma_tmp = np.diag(np.random.rand(1, doc_dim))
+		A.append(dict(mu = mu_tmp, Sigma = Sigma_tmp))
+	# initialize Rho s.t. Rho_k contains two fields "mu" (1 * V) and "Sigma" (V * V) that specifies a multivariate gaussian
+	Rho = list()
+	for k in range(K):
+		mu_tmp = np.random.rand(1, V)
+		Sigma_tmp = np.diag(np.random.rand(1, V))
+		Rho.append(dict(mu = mu_tmp, Sigma = Sigma_tmp))
+	# initialize 
+	U_prime = list()
 def load_documents(filein):
 	return B
 
