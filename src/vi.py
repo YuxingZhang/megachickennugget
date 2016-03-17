3ee6428fc1db6e59e9c913871400d05c2145d96d
Makarovski <zhangyx233@gmail.com>
2016-03-17 15:47:54 -0400

    haha

--- src/vi.py
+++ src/vi.py
@@ -3,6 +3,13 @@ import numpy as np
 
 def run():
     # Yuxing Zhang TODO
+    while true: # while not converge
+        # TODO sample a batch of document B
+        # TODO 
+        for d in B:
+            for w_dn in d:
+                # TODO update q(z_dn) by Eq.7 
+
 
     # Tianshu Ren starts here TODO
 
