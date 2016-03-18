489d7f539f1c3c45dba5af1852f035a14b321a23
Tianshu Ren <tianshu.r@gmail.com>
2016-03-18 14:31:11 -0400

    mafan

--- src/update.py
+++ src/update.py
@@ -8,23 +8,23 @@ def mylambda(xi):
 	# Helper function on Page 5 under Eq. 1
 	return 1 / (2 * xi) * (1 / (1 + exp(- xi)) - 0.5)
 
-def update_q_z(zvec, d, n, K, V, mu_d, Rho, word_idx, xi, alpha):
+def update_q_z(Z_dn, d, n, K, V, mu_d, Rho, word_idx, xi, alpha):
 	# update the vector z_dn of length K from Eq. 7
 	# q(z_dn) is a multinomial distribution with q(z_dn=k) = z_dn(k)
 
 	for z in range(K):
 		E1 = mu_d(z)
-		tmp1 = 0
-		tmp2 = 0
+		tmp = 0
+		# tmp2 = 0
 		for w in V:
-			tmp1 += mylambda(xi[z][w]) * (Rho[z]['Sigma'][w] ** 2 + Rho[z]['mu'][w] ** 2)
-			tmp2 += xi[z][w] / 2
-		E2 = Rho[z]['mu'][word_idx] + alpha[z](V / 2 - 1) - tmp1 - (0.5 - 2 * alpha[z] * 
-			mylambda(xi[z][w])) * Rho[z]['mu'][w] + tmp2 - mylambda(xi[z][w]) * 
-			(alpha[z] ** 2 - xi[z][w] ** 2) - np.log(1 + exp(xi[z][w]))
-		zvec[z] = exp(E1 + E2)
-
-	return normalize(zvec)
+			tmp += mylambda(xi[z][w]) * (Rho[z]['Sigma'][w] ** 2 + Rho[z]['mu'][w] ** 2) - (1/2 - 2 * alpha[z] * mylambda(xi[z][w])) * Rho[z]['mu'][w] \
+				   + xi[z][w] / 2 - mylambda(xi[z][w]) * (alpha[z] ** 2 - xi[z][w] ** 2) - np.log(1 + np.exp(xi[z][w]))
+			# tmp2 += xi[z][w] / 2
+		E2 = Rho[z]['mu'][word_idx] + alpha[z](V / 2 - 1) - tmp
+		Z_dn[z] = np.exp(E1 + E2)
+		Z_dn = normalize(Z_dn)
+	#
+	# return normalize(zvec)
 
 def update_eta(Eta_d, Xi_DK_d, Alpha_D_d, gamma, U, A_d):
 	for k in range(K):
