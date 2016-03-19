96383cc03fd9957bf3ef00807c44337df7c5c93c
Makarovski <zhangyx233@gmail.com>
2016-03-18 21:52:43 -0400

    easy

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,9 +1,29 @@
 import re
 import string
 
-filein = open(input_file, 'r')
-fileout = open(output_file)
-pattern = re.compile('[\W_]+')
-for line in file:
-	for word in line.strip().split():
-		new_word = pattern.sub('', word),
+
+if __name__ == '__main__':
+
+    path = '/home/johnny/Desktop/Probabilistic Graphical Models/10708 project/nipsdata/'
+    input_file = 'file_names'
+    output_file = 'corpus.txt'
+    
+    f = open(path + input_file, 'r')
+    docs = f.readlines()
+    f.close()
+    fout = open(path + output_file, 'w')
+    
+    for line in docs:
+        f = open(path + line.strip(), 'r')
+        doc = f.readlines()
+        f.close()
+        for line in doc:
+            line = re.sub(r"\d+", '', line)
+            line = re.sub(r"[^\w\s]", '', line)
+            line = re.sub(r"\s+", ' ', line)
+            if line.strip() != '':
+                fout.write(line.strip() + ' ')
+
+        fout.write('\n')
+
+    fout.close()
