c0ce6e274a7c1ac1f72bb0592ba552891aa959f6
Tianshu Ren <tianshu.r@gmail.com>
2016-04-10 19:28:58 -0400

    update

--- src/update.py
+++ src/update.py
@@ -1,7 +1,6 @@
 import numpy as np
 from sklearn.preprocessing import normalize
 from numpy.linalg import inv
-from numpy.linalg import norm
 
 ''' 
 FUNCTION ARGUMENTS ORDER:
@@ -241,8 +240,8 @@ def update_beta(U_prime, word_emb, Rho, V, K):
     tmp = 0
     for k in range(K):
         for w in range(V):
-            tmp += np.trace(np.dot(np.dot(U_prime['mu'][k], U_prime['mu'][k].transpose()) \
-            + U_prime['Sigma'], np.dot(word_emb[w], word_emb[w].transpose()))) + Rho['mu'][k][w]**2 \
+            tmp += np.trace(np.dot(np.dot(U_prime['mu'][k], U_prime['mu'][k].transpose())
+            + U_prime['Sigma'], np.dot(word_emb[w], word_emb[w].transpose()))) + Rho['mu'][k][w]**2
             + Rho['Sigma'][k][w] - 2 * np.dot(word_emb[w].transpose(), U_prime['mu'][k]) * Rho['mu'][k][w]
     return K * V / tmp
 
@@ -250,8 +249,8 @@ def update_gamma(Eta, A, U, D, K):
     tmp = 0
     for d in range(D):
         for k in range(K):
-            tmp += Eta['mu'][d][k]**2 + Eta['Sigma'][d][k] - 2 * Eta['mu'][d][k] * np.dot(U['mu'][k].transpose(), \
-                A['mu'][d]) + np.trace(np.dot(np.dot(A['mu'][d], A['mu'][d].transpose()) + A['Sigma'], \
+            tmp += Eta['mu'][d][k]**2 + Eta['Sigma'][d][k] - 2 * Eta['mu'][d][k] * np.dot(U['mu'][k].transpose(),
+                A['mu'][d]) + np.trace(np.dot(np.dot(A['mu'][d], A['mu'][d].transpose()) + A['Sigma'],
                 np.dot(U['mu'][k], U['mu'][k].transpose()) + U['Sigma']))
     return D * K / tmp
 
