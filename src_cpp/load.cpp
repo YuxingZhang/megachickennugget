e6edbb96723f2b754e62e0945e87e3d53bf443d7
Makarovski <zhangyx233@gmail.com>
2016-04-14 13:46:05 -0400

    debug

--- src_cpp/load.cpp
+++ src_cpp/load.cpp
@@ -1,5 +1,5 @@
 void load_files(string embedding, string corpus, double** word_embedding, map<string, int> word2idx,
-        string* idx2word, vector<vector<string>> W, int N) {
+        string* idx2word, vector<vector<string>> W, int* N) {
     /* param:
      *
      * */
