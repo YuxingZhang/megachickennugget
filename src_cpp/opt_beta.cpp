79b9828d541608609a79ec5e895e825f79c628a6
MuLidan <mulidan111@gmail.com>
2016-04-23 12:03:43 -0400

    update_beta

--- src_cpp/opt_beta.cpp
+++ src_cpp/opt_beta.cpp
@@ -1,41 +1,43 @@
 void UpdateBeta(vec& beta, vec& rho_m, int K, int V){
-	double eps = 0.00001;
+	double NETON_THRESH = 0.00001;
 	int MAX_ITER = 1000;
 
 	vec df(V, fill::zeros);
-	vec h(V, fill::zeros);
 	vec g(V, fill::zeros);
-	vec st(K, fill::zeros);
-	
+	vec h(V, fill::zeros);
+
+	// compute the first derivative
+	double digamma_beta = digamma(sum(beta));
+	double digamma_theta = 0;
 	for(int k = 0; k < K; k++){
-		st(k) = sum(rho_m.row(k));
+		digamma_theta += digamma(sum(rho_m.row(k)));
 	}
-	int iter = 0;
-	double c, z, sb;
-	sb = sum(beta);
+	for(int w = 0; w < V; w++){
+		double temp = 0;
+		for(int k = 0; k < K; k++){
+			temp += digamma(rho_m(k, w));
+		}
+		g(w) = K * (digamma_beta - digamma(beta(w))) + temp - digamma_theta;
+	}
+	
+	// compute the Hessian
+	double trigamma_beta = trigamma(sum(beta));
+	for(int w = 0; w < V; w++){
+		h(w) = K * trigamma(beta(w));
+	}
+
+	// compute constant terms needed for gradient
+	double c = sum(g / h) / (- 1 / trigamma_beta + sum(1 / h));
+
 	do{
-		double temp1 = 0, temp2 = 0;
-		z = ComputeZ();
-		for(int i = 0; i < V; i++){
-			g(i) = ComputeG();
-			h(i) = ComputeH();
-			temp1 += 1 / h(i);
-			temp2 += g(i) / h(i);
-		}
-		c = temp2 / (1 / z + temp1);
+		iter++;
 
-		for(int i = 0; i < V; i++){
-			df(i) = (g(i) - c) / h(i);
+		for(int w = 0; w < V; w++){
+			df(w) = (g(i) - c) / h(i);
 		}
-
+		
 		beta -= df;
 	} while(iter < MAX_ITER && max(abs(df)) > eps);
 
 	return;
 }
-
-double ComputeH(double beta_w, int K){
-	return K * trigamma(beta_w);
-}
-
-double ComputeG(double sb, double st_k, double beta_w, double theta_kw, int K)
