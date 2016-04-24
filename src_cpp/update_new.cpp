e31031dd5cab85ae94254f7179886b603ba9fca6
MuLidan <mulidan111@gmail.com>
2016-04-23 20:20:58 -0400

    iii

--- src_cpp/update_new.cpp
+++ src_cpp/update_new.cpp
@@ -12,7 +12,7 @@ bool UpdateZ(int d, int n, vector<mat>& z, mat& eta_m, mat& rho_m, vector< vecto
 
     for (int k = 0; k < K; k++) {
         double E1 = eta_m(d, k);
-        double E2 = digamma(rho_m(k, word2idx[w_dn])) - digamma(sum(rho_m.row(k)));
+        double E2 = digammal(rho_m(k, word2idx[w_dn])) - digammal(sum(rho_m.row(k)));
         ///cout << "E1 = " << E1 << " , E2 = " << E2 << endl;
         z[d](n, k) = exp(E1 + E2);
     }
