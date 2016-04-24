308f63b00b650f6bef25a4c4548873beaddc7949
Tianshu Ren <tianshu.r@gmail.com>
2016-04-23 20:00:48 -0400

    0

--- src_cpp/opt_beta.cpp
+++ src_cpp/opt_beta.cpp
@@ -6,31 +6,31 @@ vec UpdateBeta(vec& beta, mat& rho_m, int V, int K){
 	vec df(V, fill::zeros);
 	vec g(V, fill::zeros);
 	vec h(V, fill::zeros);
-
-	// compute the first derivative
-	double digamma_beta = digamma(sum(beta));
-	double digamma_theta = 0;
-	for(int k = 0; k < K; k++){
-		digamma_theta += digamma(sum(rho_m.row(k)));
-	}
-	for(int w = 0; w < V; w++){
-		double temp = 0;
-		for(int k = 0; k < K; k++){
-			temp += digamma(rho_m(k, w));
-		}
-		g(w) = K * (digamma_beta - digamma(beta(w))) + temp - digamma_theta;
-	}
-	
-	// compute the Hessian
-	double trigamma_beta = trigamma(sum(beta));
-	for(int w = 0; w < V; w++){
-		h(w) = K * trigamma(beta(w));
-	}
-
-	// compute constant terms needed for gradient
-	double c = sum(g / h) / (- 1 / trigamma_beta + sum(1 / h));
 	int iter = 0;
 	do{
+		// compute the first derivative
+		double digamma_beta = digamma(sum(beta));
+		double digamma_theta = 0;
+		for(int k = 0; k < K; k++){
+			digamma_theta += digamma(sum(rho_m.row(k)));
+		}
+		for(int w = 0; w < V; w++){
+			double temp = 0;
+			for(int k = 0; k < K; k++){
+				temp += digamma(rho_m(k, w));
+			}
+			g(w) = K * (digamma_beta - digamma(beta(w))) + temp - digamma_theta;
+		}
+		
+		// compute the Hessian
+		double trigamma_beta = trigamma(sum(beta));
+		for(int w = 0; w < V; w++){
+			h(w) = K * trigamma(beta(w));
+		}
+
+		// compute constant terms needed for gradient
+		double c = sum(g / h) / (- 1 / trigamma_beta + sum(1 / h));
+		
 		iter++;
 
 		for(int w = 0; w < V; w++){
