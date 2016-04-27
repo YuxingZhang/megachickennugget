2489b01e7c6e686dea65d7ca434a7f4d59278156
Makarovski <zhangyx233@gmail.com>
2016-04-27 15:53:07 -0400

    yuan di ta bu...... zou!

--- src_sample/sample_eta.cpp
+++ src_sample/sample_eta.cpp
@@ -1,37 +1,33 @@
 #include <random>
-
-void SampleEta(int d, vec& eta_m, vec& u_m, vec& a_m, vector<mat>& z, int N, int K){
+void SampleEta(int d, mat& eta_m, mat& u_m, mat& a_m, vector<mat>& z, int N, int K){
 	// initialize the value of eta
-	double eta_sample, new_eta, old_eta, sum_eta, C, mu, sigma, accept;
-	sum_eta = sum(eta_m);
+	double eta_sample, new_eta, old_eta, C, mu, sigma, accept;
 	std::default_random_engine generator;
+        int iterations = 1000;
+
+	// proposal distribution
+        double proposal_mu[K];
+        for (int k = 0; k < K; k++) {
+            proposal_mu[k] = dot(u_m.row(k), a_m.row(d)) + z[d](n, k) / gamma;
+        }
+        double proposal_sigma = 1 / gamma;
 
 	// Gibbs sampling
-	for(int k = 0; k < K; k++){
-		int iter = 0;
-		old_eta = eta_sample(k);
-		C = sum_eta - eta_m(k);
-		eta_sample = 0;
-		for(iter < 1000){
-			iter++;
-			// propose from the normal distribution
-			sigma = 1 / gamma;
-			mu = dot(u_m.row(i), a_m.row(d)) + z[d](n, i) / gamma;
-  			normal_distribution<double> distribution(mu, sigma);
-  			new_eta = distribution(generator);
+        for (int i = 0; i < iterations; i++){
+            for (int k = 0; k < K; k++) {
+                normal_distribution<double> distribution(proposal_mu, proposal_sigma);
+                // propose from the normal distribution
+                new_eta = distribution(generator);
+                // compute the rejection criteria
+                accept = min((C + exp(new_eta)) / (C + exp(old_eta)), 1);
+                // get the new sample
+                uniform_real_distribution<double> distribution(0.0, 1.0);
+                double prob = distribution(generator);
+                if(prob > accept) new_eta = old_eta;
+                eta_sample_sum += new_eta;
+                old_eta = new_eta;
+            }
+        }
 
-			// compute the rejection criteria
-  			accept = min((C + exp(new_eta)) / (C + exp(old_eta)), 1);
-
-			// get the new sample
-			uniform_real_distribution<double> distribution(0.0, 1.0);
-			double prob = distribution(generator);
-			if(prob > accept) new_eta = old_eta;
-
-			eta_sample += new_eta;
-			old_eta = new_eta;
-		}
-		eta_m(k) = eta_sample / 1000;
-	}
 	return;
 }
