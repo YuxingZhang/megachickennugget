d2efbf06de6e474e893e30e4c40849024a94dcf8
Makarovski <zhangyx233@gmail.com>
2016-04-27 16:13:17 -0400

    人总有原地踏步的时候，然而有的老年人只会原地踏步

--- src_sample/sample_eta.cpp
+++ src_sample/sample_eta.cpp
@@ -1,24 +1,26 @@
 #include <random>
 void SampleEta(int d, mat& eta_m, mat& u_m, mat& a_m, vector<mat>& z, double gamma, int N, int K){
-	// initialize the value of eta
-	vec prev_eta = eta_m.row(d);
+    // initialize the value of eta
+    vec prev_eta = eta_m.row(d);
     vec eta_sample_sum(K, fill::zeros);
-    double new_eta, C, mu, sigma, accept;
-	std::default_random_engine generator;
+    double new_eta, sum_eta, accept;
+    std::default_random_engine generator;
     int iterations = 1000;
     int num_sample = 100;
 
-	// proposal distribution
+    // proposal distribution
     double proposal_mu[K];
     for (int k = 0; k < K; k++) {
         proposal_mu[k] = dot(u_m.row(k), a_m.row(d)) + z[d](n, k) / gamma;
     }
     double proposal_sigma = 1 / gamma;
 
-	// Gibbs sampling
+    // Gibbs sampling
+    sum_eta = sum(exp(prev_eta));
     for (int i = 0; i < iterations; i++){
         for (int k = 0; k < K; k++) {
-            C = sum(prev_eta) - prev_eta(k);
+            C = sum_eta - exp(prev_eta(k));
+            
             normal_distribution<double> distribution(proposal_mu, proposal_sigma);
             // propose from the normal distribution
             new_eta = distribution(generator);
@@ -28,14 +30,15 @@ void SampleEta(int d, mat& eta_m, mat& u_m, mat& a_m, vector<mat>& z, double gam
             uniform_real_distribution<double> distribution(0.0, 1.0);
             double prob = distribution(generator);
             if(prob > accept) new_eta = prev_eta(k);
-            
+
             if(i > iterations - num_sample){
                 eta_sample_sum(k) += new_eta;
             }
+            sum_eta = sum_eta - prev_eta(k) + new_eta;
             prev_eta(k) = new_eta;
         }
     }
     eta_m.row(d) = eta_sample_sum.t() / num_sample;
 
-	return;
+    return;
 }
