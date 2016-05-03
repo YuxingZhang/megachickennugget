11dda02b08c824c4998114886d6076c67f705377
Tianshu Ren <tianshu.r@gmail.com>
2016-05-03 16:05:22 -0400

    added update alpha

--- src_lda/update.cpp
+++ src_lda/update.cpp
@@ -108,25 +108,24 @@ void UpdateAlpha(vec& alpha, mat& gamma, int K, int D) {
     int MAX_ITER = 1000;
     double gamma = 0.001;
 
+    double c = 0.0;
+    double z = 0.0;
     double g [K] = { };
     double h [K] = { };
     double update [K] = { };
+    
+    double sum_gamma [D] = { };
+    for (int d = 0; d < D; d++){
+        sum_gamma [d] = sum(gamma.row(d));
+    }
+
     int iter = 0;
 
     do{
-        double c = 0.0;
-        double z = 0.0;
-
         double elbo_old = 0.0;
         double elbo_new = 0.0;
-
         double sum_alpha = sum(alpha);
-        double sum_gamma [D] = { };
         double max_df = 0.0;
-
-        for (int d = 0; d < D; d++){
-            sum_gamma [d] = sum(gamma.row(d));
-        }
 
         for (int i = 0; i < K; i++){
             double tmp = 0.0;
@@ -172,6 +171,7 @@ void UpdateAlpha(vec& alpha, mat& gamma, int K, int D) {
             elbo_new += lgamma(sum_alpha) + tmp5;
         }    
         cout << "elbo after alpha update: " << elbo_new << endl;
+        
     } while (iter < MAX_ITER && max_df > NEWTON_THRESH);
 
     return;
